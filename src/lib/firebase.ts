22529b6e7e1c2b5bb96888b08b1347047547b235
Quasi Ahama <quasiahama@gmail.com>
2025-10-01 03:01:09 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-8660039264-bcf77",
-  "appId": "1:458115625281:web:dd0ee2d312559ef93e71c0",
-  "apiKey": "AIzaSyBdyy82iA0-M1arGRk9IUSKtNt097oiA4s",
-  "authDomain": "studio-8660039264-bcf77.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "458115625281"
+  projectId: "studio-8660039264-bcf77",
+  appId: "1:458115625281:web:dd0ee2d312559ef93e71c0",
+  apiKey: "AIzaSyBdyy82iA0-M1arGRk9IUSKtNt097oiA4s",
+  authDomain: "studio-8660039264-bcf77.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "458115625281"
 };
 
 // Initialize Firebase
