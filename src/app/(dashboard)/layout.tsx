49e0947d6fa8bb03a0e7d308d9364fad7a1b634a
Quasi Ahama <quasiahama@gmail.com>
2025-10-01 13:53:05 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,19 +15,9 @@ export default function DashboardRootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={cn('font-body antialiased bg-muted/40')}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <div className={cn('font-body antialiased bg-muted/40')}>
+      {children}
+      <Toaster />
+    </div>
   );
 }
