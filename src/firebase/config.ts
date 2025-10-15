44de62c0717e2199fe54237f7a061fc3b1e9573d
Quasi Ahama <quasiahama@gmail.com>
2025-10-15 15:44:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8660039264-bcf77",
+  "appId": "1:458115625281:web:dd0ee2d312559ef93e71c0",
+  "apiKey": "AIzaSyBdyy82iA0-M1arGRk9IUSKtNt097oiA4s",
+  "authDomain": "studio-8660039264-bcf77.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "458115625281"
+};
